7fd10f5af728131b221afe3f4d736776358a96a6
XINQI CAO <xiaoyesaya233@gmail.com>
2025-03-09 19:05:36 -0700

    filter class update

--- src/main/java/student/Filters.java
+++ src/main/java/student/Filters.java
@@ -75,7 +75,14 @@ public final class Filters {
                 return !gameData.equalsIgnoreCase(value);
             case CONTAINS:
                 return gameData.toLowerCase().contains(value.toLowerCase());
-
+            case GREATER_THAN:
+                return gameData.compareToIgnoreCase(value) > 0;
+            case LESS_THAN:
+                return gameData.compareToIgnoreCase(value) < 0;
+            case GREATER_THAN_EQUALS:
+                return gameData.compareToIgnoreCase(value) >= 0;
+            case LESS_THAN_EQUALS:
+                return gameData.compareToIgnoreCase(value) <= 0;
             default:
                 return false;
         }
